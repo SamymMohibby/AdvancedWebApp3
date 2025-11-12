7d83ec800b3905b2236b957d1c83305a85715d6b
SamymMohibby <samymmohibby@gmail.com>
2025-11-12 09:10:47 +0200

    first commit

---
+++ public/main.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("userForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    const user = { name, email };
+
+    const res = await fetch("/user", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(user),
+    });
+})
+
+const userBtn = document.getElementById("getUsers")
+const userList = document.getElementById("userList")
+userBtn.addEventListener("click", async () => {
+  try {
+    const response = await fetch("/users");
+    const data = await response.json();
+    console.log("GET /users response:", data);
+    console.log(data.length)
+    for (let i = 0; i < data.length; i++) {
+        const node = document.createElement("li")
+        const textNode = document.createTextNode(`${data[i].name} - ${data[i].email}`)
+        node.appendChild(textNode)
+        userList.appendChild(node)
+    }
+  } catch (err) {
+    console.error("Error fetching users:", err);
+  }
+});
---
+++ server.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response, Express } from "express";
+import path from "path"
+
+const app: Express = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "../public")))
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get("/hello", (req,res) => {
+    res.json({msg: "Hello world!"})
+})
+
+app.get("/echo/:id", (req, res) => {
+    res.json({id: req.params.id})
+})
+
+let numbers: number[] = [4, 8, 7, 13, 12]
+let sum: number = 0;
+
+numbers.forEach(x => {
+    sum += x;
+});
+
+
+app.post("/sum", (req, res) => {
+    let numbers:number[] =  req.body.numbers
+    let sum: number = 0;
+
+    numbers.forEach(x => {
+        sum += x;
+    });
+    res.json({sum: sum})
+})
+
+type TUser = {
+    name: string,
+    email: string
+}
+
+const users: TUser[] = []
+
+app.post("/user", (req, res) => {
+    const user: TUser = req.body
+    users.push(user)
+    res.json({message: "User added successfully", user})
+    
+})
+
+app.get("/users", (_req: Request, res: Response) => {
+  res.json(users);
+});
