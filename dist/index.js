913c9c28b8ede2cfec754499b87401cae437f2ac
SamymMohibby <samymmohibby@gmail.com>
2025-11-12 18:27:06 +0200

    renamed server.ts to index.ts

---
+++ dist/index.js
@@ -0,0 +1,43 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.use(express_1.default.json());
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+app.get("/hello", (req, res) => {
+    res.json({ msg: "Hello world!" });
+});
+app.get("/echo/:id", (req, res) => {
+    res.json({ id: req.params.id });
+});
+let numbers = [4, 8, 7, 13, 12];
+let sum = 0;
+numbers.forEach(x => {
+    sum += x;
+});
+app.post("/sum", (req, res) => {
+    let numbers = req.body.numbers;
+    let sum = 0;
+    numbers.forEach(x => {
+        sum += x;
+    });
+    res.json({ sum: sum });
+});
+const users = [];
+app.post("/users", (req, res) => {
+    const user = req.body;
+    users.push(user);
+    return res.json({ message: "User successfully added" });
+});
+app.get("/users", (_req, res) => {
+    return res.status(201).json(users);
+});
+//# sourceMappingURL=index.js.map
